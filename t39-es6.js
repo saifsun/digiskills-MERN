791c674b27ddaae3b020ddbcda9339c44ab9f2f1
Saifullah <saifullahabbas@gmail.com>
2026-09-15 00:25:12 +0500

    learned t38

---
+++ t39-es6.js
@@ -0,0 +1,25 @@
+
+// const name = "Saif";
+// console.log("name =", name); //old way
+// console.log(`name = ${name}`); //template literals
+
+// let email = `Hello dear!
+// This is an email I am sending to you,
+// to remind you of me.
+// Thanks`;
+
+// console.log(email);
+
+
+//array destructure
+// let fruits = ["banana", "mango", "orange"];
+// let [first,second] = fruits;
+// console.log(first, second);
+
+// let fruits = ["banana", "mango", "orange"];
+// let [first, , second] = fruits;  //skipping mango
+// console.log(first, second);
+
+
+
+
